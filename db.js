0d85bb6c2c65737260d3c3ef7ea6a3efb30eedc4
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2026-03-23 00:14:41 +0800

    parsed port

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const connection = await mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  port: process.env.MYSQL_PORT,
+  port: parseInt(process.env.MYSQL_PORT, 10),
   database: process.env.MYSQL_DATABASE,
 });
 
