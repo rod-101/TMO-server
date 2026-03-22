27c0223cfa62d87db27a0c00c61865552a3810cb
Rodjones Rosalinda <rodjonesrosalinda@gmail.com>
2026-03-23 00:36:48 +0800

    modified db to async

--- db.js
+++ db.js
@@ -2,12 +2,17 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connection = await mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  port: parseInt(process.env.MYSQL_PORT, 10),
-  database: process.env.MYSQL_DATABASE,
-});
+async function initDB() {
+  const connection = await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port: parseInt(process.env.MYSQL_PORT, 10),
+    database: process.env.MYSQL_DATABASE,
+  });
 
+  return connection;
+}
+
+const connection = await initDB(); // now dotenv is guaranteed loaded
 export default connection;
